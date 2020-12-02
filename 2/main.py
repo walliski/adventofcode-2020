a235443e608f0b1a421909b573db06098522a107
Walliski <walliski@gmail.com>
2020-12-02 21:53:34 +0200

    Removed some extra lines

--- 2/main.py
+++ 2/main.py
@@ -3,12 +3,10 @@
 with open('input.txt', 'r') as content:
     lines = content.readlines()
 
-validCount = 0
-validCount2 = 0
+validCount, validCount2 = 0, 0
 
 for line in lines:
-    splitLine = line.replace(":", "")
-    minMaxCounts, letter, password = splitLine.split(" ")
+    minMaxCounts, letter, password = line.replace(":", "").split(" ")
     minCount, maxCount = minMaxCounts.split("-")
 
     if int(minCount) <= password.count(letter) <= int(maxCount):
