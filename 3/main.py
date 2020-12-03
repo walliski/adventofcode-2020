0fcdf1cd6f2c4958a2a8b5bb93761170feb2d64b
Walliski <walliski@gmail.com>
2020-12-03 22:41:53 +0200

    Split results into own dictionary
    So that we do not need to modify the input dictionary at all.

--- 3/main.py
+++ 3/main.py
@@ -4,30 +4,30 @@ with open('input.txt', 'r') as content:
 slopes = {
     "1x1": {
         "x": 1,
-        "y": 1,
-        "count": 0
+        "y": 1
     },
     "3x1": {
         "x": 3,
-        "y": 1,
-        "count": 0
+        "y": 1
     },
     "5x1": {
         "x": 5,
-        "y": 1,
-        "count": 0
+        "y": 1
     },
     "7x1": {
         "x": 7,
-        "y": 1,
-        "count": 0
+        "y": 1
     },
     "1x2": {
         "x": 1,
-        "y": 2,
-        "count": 0
+        "y": 2
     }
 }
+
+treeCounts = {}
+
+for slope in slopes:
+    treeCounts[slope] = 0
 
 width = len(lines[0])
 
@@ -36,12 +36,12 @@ for i, line in enumerate(lines):
         # Adding + 1 for i, since we are starting to move from the first line, so if we move 2 lines down, we will end
         # up on line 3, not 2. :thinking:
         if (i + 1) % data["y"] == 0 and line[(i * data["x"]) % (width - 1)] == "#":
-            data["count"] += 1
+            treeCounts[slope] += 1
 
 multiplied = 1
 
-for slope, data in slopes.items():
-    print("Tree count for ", slope, ": ", data["count"])
-    multiplied *= data["count"]
+for slope in treeCounts:
+    print("Tree count for ", slope, ": ", treeCounts[slope])
+    multiplied *= treeCounts[slope]
 
-print("Mutliplied count of all slopes: ", multiplied)
+print("Multiplied count of all slopes: ", multiplied)
