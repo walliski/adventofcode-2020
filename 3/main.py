565d21676eeed7d64cc06eeac85506673d689c81
Walliski <walliski@gmail.com>
2020-12-03 22:49:40 +0200

    Use array instead of dictionary for slopes
    No need to duplicate the x,y data in it, when it can be easily parsed
    from the name of the slope itself.

--- 3/main.py
+++ 3/main.py
@@ -1,41 +1,20 @@
 with open('input.txt', 'r') as content:
     lines = content.readlines()
 
-slopes = {
-    "1x1": {
-        "x": 1,
-        "y": 1
-    },
-    "3x1": {
-        "x": 3,
-        "y": 1
-    },
-    "5x1": {
-        "x": 5,
-        "y": 1
-    },
-    "7x1": {
-        "x": 7,
-        "y": 1
-    },
-    "1x2": {
-        "x": 1,
-        "y": 2
-    }
-}
+slopes = ["1x1", "3x1", "5x1", "7x1", "1x2"]
 
+width = len(lines[0])
 treeCounts = {}
 
 for slope in slopes:
     treeCounts[slope] = 0
 
-width = len(lines[0])
-
 for i, line in enumerate(lines):
-    for slope, data in slopes.items():
+    for slope in slopes:
+        x, y = map(int, slope.split("x"))
         # Adding + 1 for i, since we are starting to move from the first line, so if we move 2 lines down, we will end
         # up on line 3, not 2. :thinking:
-        if (i + 1) % data["y"] == 0 and line[(i * data["x"]) % (width - 1)] == "#":
+        if (i + 1) % y == 0 and line[(i * x) % (width - 1)] == "#":
             treeCounts[slope] += 1
 
 multiplied = 1
