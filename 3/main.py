b22f0f0f3233ddf40e7f6efbd2599ce70678f21b
Walliski <walliski@gmail.com>
2020-12-03 23:03:00 +0200

    Flip loops and remove unneeded code
    With the loops this way around, we can do the calculations and setup in
    the main loop itself, which removes the need to loop over things a lot.

--- 3/main.py
+++ 3/main.py
@@ -2,25 +2,21 @@ with open('input.txt', 'r') as content:
     lines = content.readlines()
 
 slopes = ["1x1", "3x1", "5x1", "7x1", "1x2"]
-
 width = len(lines[0])
-treeCounts = {}
+multiplied = 1
 
 for slope in slopes:
-    treeCounts[slope] = 0
+    treeCount = 0
 
-for i, line in enumerate(lines):
-    for slope in slopes:
+    for i, line in enumerate(lines):
         x, y = map(int, slope.split("x"))
+
         # Adding + 1 for i, since we are starting to move from the first line, so if we move 2 lines down, we will end
         # up on line 3, not 2. :thinking:
         if (i + 1) % y == 0 and line[(i * x) % (width - 1)] == "#":
-            treeCounts[slope] += 1
+            treeCount += 1
 
-multiplied = 1
-
-for slope in treeCounts:
-    print("Tree count for ", slope, ": ", treeCounts[slope])
-    multiplied *= treeCounts[slope]
+    print("Tree count for ", slope, ": ", treeCount)
+    multiplied *= treeCount
 
 print("Multiplied count of all slopes: ", multiplied)
