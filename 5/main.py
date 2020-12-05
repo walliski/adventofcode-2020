95fd59b143ddaede12adaddf96670bfca2ad3382
Walliski <walliski@gmail.com>
2020-12-05 17:01:17 +0200

    Add solution for Day 5 Step 2
    Not the most optimal one performance wise, since I do a lot of lookups
    in the array.
    
    Also apparently running max on a map output, works... But if you run min
    afterwards, the map is empty. So need to make that a list first it
    seems.

--- 5/main.py
+++ 5/main.py
@@ -1,7 +1,13 @@
 with open('input.txt', 'r') as content:
     lines = content.readlines()
 
-# b1 f0
-# r1 l0
+# We can just convert the numbers into binary like this: b->1, f->0 and r->1, l->0
+seats = list(map((lambda x: int(x[:7].replace("B","1").replace("F","0"), 2) * 8 + int(x[7:].replace("R","1").replace("L","0"), 2)), lines))
+print("Step 1: ", max(seats))
 
-print("Step 1: ", max(map((lambda x: int(x[:7].replace("B","1").replace("F","0"), 2) * 8 + int(x[7:].replace("R","1").replace("L","0"), 2)), lines)))
+# Check through all seats and see if the one before and after are taken, but middle on is not.
+# Not really optimal I guess, a lot of lookups. Sorting and finding a hole might be more efficient.
+for i in range(min(seats), max(seats)):
+    if (i - 1 in seats) and not (i in seats) and (i + 1 in seats):
+        print("Step 2: ", i)
+        break
