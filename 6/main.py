70d25be19234b6f29542259762478c82ac3e54b0
Walliski <walliski@gmail.com>
2020-12-06 22:22:56 +0200

    Add solution for Day 6 Step 2

--- 6/main.py
+++ 6/main.py
@@ -3,12 +3,24 @@ with open('input.txt', 'r') as content:
 
 charCount = {}
 answerCounts = 0
+groupCount = 0
+everyoneCount = 0
 
 for line in lines:
     if line.isspace():
+        # Calculate Step 1 sum
         answerCounts += len(charCount.keys())
-        print(charCount)
+
+        # Calculate step 2 count
+        for cCount in charCount.values():
+            if cCount == groupCount:
+                everyoneCount += 1
+
+        # Reset
         charCount = {}
+        groupCount = 0
+    else:
+        groupCount += 1
 
     for char in line.strip():
         if char not in charCount:
@@ -16,3 +28,4 @@ for line in lines:
         charCount[char] += 1
 
 print("Step 1 sum: ", answerCounts)
+print("Step 2 sum: ", everyoneCount)
