92d2b2b6871c9458f32fbd8811e6c6672d3032a7
Walliski <walliski@gmail.com>
2020-12-08 21:11:17 +0200

    Remove unused code
    Added this as I misread the assignment at first. Not needed it seems, so
    lets go on...

--- 8/main.py
+++ 8/main.py
@@ -2,14 +2,12 @@ with open('input.txt', 'r') as content:
     lines = content.readlines()
 
 opCount = [0] * len(lines)
-insHistory = []
 curInstruction = 0
 acc = 0
 
 while (opCount[curInstruction] < 1):
     op, arg = lines[curInstruction].strip().split(" ")
 
-    insHistory.append(curInstruction)
     opCount[curInstruction] += 1
 
     if op == "acc":
@@ -20,6 +18,4 @@ while (opCount[curInstruction] < 1):
 
     curInstruction += 1
 
-
 print("Accumulator value before loop exited: ", acc)
-print("Instruction before loop was entered: ", insHistory[-1])
