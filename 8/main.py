99b20f084381c1c004a1669753acb960ab21b7b0
Walliski <walliski@gmail.com>
2020-12-08 21:23:02 +0200

    Make running a program a function
    So that we can run different programs, starting from different states.

--- 8/main.py
+++ 8/main.py
@@ -1,21 +1,38 @@
 with open('input.txt', 'r') as content:
     lines = content.readlines()
 
-opCount = [0] * len(lines)
-curInstruction = 0
-acc = 0
+# Exitcode 1 means that we crashed in a loop
+initialState = {
+    "curInstruction": 0,
+    "acc": 0,
+    "opCount": [0] * len(lines),
+    "exitCode": 0
+}
 
-while (opCount[curInstruction] < 1):
-    op, arg = lines[curInstruction].strip().split(" ")
+def run_program(program, state):
+    opCount = state["opCount"]
+    curInstruction = state["curInstruction"]
+    acc = state["acc"]
 
-    opCount[curInstruction] += 1
+    while (opCount[curInstruction] < 1):
+        op, arg = lines[curInstruction].strip().split(" ")
 
-    if op == "acc":
-        acc += int(arg)
-        curInstruction += 1
-    elif op == "jmp":
-        curInstruction += int(arg)
-    elif op == "nop":
-        curInstruction += 1
+        opCount[curInstruction] += 1
 
-print("Accumulator value before loop exited: ", acc)
+        if op == "acc":
+            acc += int(arg)
+            curInstruction += 1
+        elif op == "jmp":
+            curInstruction += int(arg)
+        elif op == "nop":
+            curInstruction += 1
+
+    return {
+        "curInstruction": curInstruction,
+        "acc": acc,
+        "opCount": opCount,
+        "exitCode": 1
+    }
+
+crash = run_program(lines, initialState)
+print("Accumulator value before loop exited: ", crash["acc"])
