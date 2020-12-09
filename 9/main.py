fac722b0d8e5db841132347ca92ef1dbb3198086
Walliski <walliski@gmail.com>
2020-12-09 22:27:22 +0200

    Add solution for Day 9, step 2

--- 9/main.py
+++ 9/main.py
@@ -2,7 +2,6 @@ with open('input.txt', 'r') as content:
     lines = list(map(int, content.readlines()))
 
 def is_valid(pre, preCount, target):
-    print(pre)
     pre.sort()
     leftPointer = 0
     rightPointer = len(pre) - 1
@@ -18,10 +17,30 @@ def is_valid(pre, preCount, target):
 
     return False
 
+# Calculate first part
 preCount = 25
 maxCount = len(lines)
+broken = 0
 
 for i in range(preCount, maxCount):
     if not is_valid(lines[i-preCount:i], preCount, lines[i]):
-        print("Broken: ", lines[i], "on index", i)
+        broken = lines[i]
+        print("Broken: ", broken, "on index", i)
+        break
+
+# Calculate second part
+for i in range(maxCount-1):
+    sumValue = lines[i]
+    minValue = lines[i]
+    maxValue = 0
+
+    j = i+1
+    while sumValue < broken and j < maxCount:
+        sumValue += lines[j]
+        minValue = min(minValue, lines[j])
+        maxValue = max(maxValue, lines[j])
+        j += 1
+
+    if sumValue == broken:
+        print("Min number: ", minValue, "Max value:", maxValue, "sum:", minValue + maxValue)
         break
