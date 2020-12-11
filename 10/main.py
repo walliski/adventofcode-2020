c1474ef2bfa06b7055c3b1bd85b76d33ea3c50dc
Walliski <walliski@gmail.com>
2020-12-11 17:24:32 +0200

    Add solution for Day 10, Step 2
    I did not come up with this. Was locked into thinking in wrong
    solutions, and threw in the towel and got some help from the interwebs.

--- 10/main.py
+++ 10/main.py
@@ -1,4 +1,4 @@
-with open('input_test.txt', 'r') as content:
+with open('input.txt', 'r') as content:
     lines = list(map(int, content.readlines()))
 
 # Add the first and last:
@@ -15,3 +15,26 @@ for i in range(len(lines)-1):
 
 print("Diff counts:", diffs)
 print("Step 1 answer:", diffs["1"] * diffs["3"])
+
+
+# Part 2
+
+# Not my solution, I got stuck at trying to look forward and figure out the amount of combinations like that.
+# I was also looking at one element at a time, and while I figured out that that wont work in all cases, I still
+# could not figure out how to take them all into consideration...
+
+# Here is a solution borrowed from the great internet though:
+
+from collections import Counter
+
+# Counter is like a dict, but returns 0 by default if element not set.
+combs = Counter()
+
+# There is 1 way to get to the first element
+combs[0] = 1
+
+# 1: because we have the value for 0 already.
+for line in lines[1:]:
+    combs[line] = combs[line-1] + combs[line-2] + combs[line-3]
+
+print("Step 2 answer:", combs[lines[-1]])
